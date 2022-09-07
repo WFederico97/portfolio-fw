4e6d5fcca623265f9fb85f91f7f0e9b23d389276
Federico W <wuthrich09@gmail.com>
2022-09-07 17:41:19 -0300

    FIX: FRONT END

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Main from './Components/Main/Main';
 
+
 function App() {
   return (
     <>
-      <Header  />
-      <Main/>
-      <Footer  />
+      <div className='App'>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+
     </>
   );
 }
