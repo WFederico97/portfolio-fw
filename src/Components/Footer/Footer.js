0834ddebaa89588b41667becb841c92c3ba4e0f5
Federico W <wuthrich09@gmail.com>
2022-09-07 17:39:57 -0300

    FIX: FOOTER

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,24 +3,23 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './Footer.css'
+
 const Footer = () => {
     return (
         <>
             <Container fluid className="footerInfo ">
                 <Row className=" justify-content-center mx-auto  col-6 d-flex p-1 ">
-                    <Col className="col-3 footerText">
+                    <Col className="col-6  footerText">
                         <h6>© 2022 Federico W. </h6>
                     </Col>
                     <hr />
-                    <span  />
-                    <Col className="col-2  ">
-                        <img className="img-fluid " src="./Images/icons8-argentina-64.png" alt="arg icon" style={{width: '3em', height: '3em'}} />
+                    <span />
+                    <Col className="col-1  ">
+                        <a href="#about"><img className="img-fluid " src="./Images/icons8-argentina-64.png" alt="arg icon" style={{ width: '100%', height: '3em' }} /></a>
                     </Col>
                 </Row>
             </Container>
         </>
-
-
     )
 }
 export default Footer
